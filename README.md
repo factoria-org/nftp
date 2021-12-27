84f8267c495afc92153d4ef9931d9805c2021d07
skogard <skogard@protonmail.com>
2021-12-27 01:09:08 -0500

    init

---
+++ README.md
@@ -0,0 +1,64 @@
+# NFTP
+
+The simplest way to publish files and folders to IPFS, with one command.
+
+100% FREE to upload as much files as you want, powered by [nft.storage](https://nft.storage).
+
+![nftp.gif](nftp.gif)
+
+---
+
+# Install
+
+```
+npm install -g nftp
+```
+
+# Usage
+
+```
+nftp <path>
+```
+
+The first time you run the command it will ask you for the **NFT.STORAGE API KEY**. You can get it over here: https://nft.storage/
+
+Currently NFTP supports two types of uploads:
+
+1. **File upload:** specify the file path
+2. **Folder upload:** specify the path to the folder. (NOTE: NFTP currently does NOT support nested folders, only supports a folder made up of files)
+
+---
+
+# Examples
+
+For example if you want to publish all files at current path, simply run:
+
+```
+nftp .
+```
+
+To publish a specific file named `package.json`
+
+```
+nftp package.json
+```
+
+Supports absolute paths:
+
+```
+nftp /etc
+```
+
+And supports relative paths:
+
+```
+nftp ../images
+```
+
+---
+
+# FAQ
+
+1. **Is it free?:** Yes it's 100% FREE, powered by [nft.storage](https://nft.storage). NFTP is simply a program that lets you upload easily to IPFS using nft.storage
+2. **Where is my nft.storage API key stored?:** It is stored under `~/.nftstorage` (a hidden file named `.nftstorage` at your home path)
+3. **How do I refresh my nft.storage API key?:** Simply delete the `~/.nftstorage` file and run the `nftp` command again and it will ask you for a new API key. 
---
+++ index.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+process.removeAllListeners('warning')
+const { I } = require('ipfsio')
+const inquirer = require('inquirer')
+const homedir = require('os').homedir();
+const path = require('path')
+const ora = require('ora');
+const fs = require('fs')
+const keyPath = homedir + "/.nftstorage";
+const upload = async (key, target) => {
+  const i = new I(key)
+  let targetPath = path.resolve(process.cwd(), target)
+  const stat = await fs.promises.lstat(targetPath)
+  let cid
+  let type
+  if (stat.isDirectory()) {
+    type = "directory"
+    cid = await i.folder(targetPath)
+  } else if (stat.isFile()) {
+    type = 'file'
+    cid = await i.file(targetPath)
+  } else {
+    console.log("error", stat)
+  }
+  return { cid, type }
+}
+const render = (r) => {
+  if (r.cid) {
+    console.log("########################################################################################################")
+    console.log("#")
+    console.log("# IPFS CID: " + r.cid)
+    if (r.type === 'directory') {
+      console.log("# IPFS URI: ipfs://" + r.cid + "/")
+    } else if (r.type === 'file') {
+      console.log("# IPFS URI: ipfs://" + r.cid)
+    }
+    console.log("# IPFS Gateway: https://ipfs.io/ipfs/" + r.cid)
+    console.log("#")
+    console.log("########################################################################################################")
+  }
+}
+if (process.argv.length < 3) {
+  console.log("please enter the file/folder path to upload.")
+  process.exit(1)
+}
+const target = process.argv[2]
+if (fs.existsSync(keyPath)) {
+  let key = fs.readFileSync(keyPath, "utf8")
+  const throbber = ora('uploading').start();
+  console.time("upload duration")
+  upload(key, target).then((r) => {
+    throbber.stop();
+    render(r)
+    console.log("")
+    console.timeEnd("upload duration")
+  })
+} else {
+  inquirer.prompt([{
+    type: 'input',
+    name: "key",
+    message: 'NFT.STORAGE API KEY',
+  }]).then((answers) => {
+    fs.writeFileSync(keyPath, answers.key)
+    console.time("upload duration")
+    upload(key, target).then((r) => {
+      throbber.stop();
+      render(r)
+      console.log("")
+      console.timeEnd("upload duration")
+    })
+  })
+}
